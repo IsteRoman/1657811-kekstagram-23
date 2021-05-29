bffc06ed3946b648e6fed3c5deb51dbb79c702b5
RomanIst <demonatr@mail.ru>
2021-05-29 20:53:19 +0500

    Выполнение второго домашнего задания 2

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 const gettingValue = function(minValue, maxValue) {
   if (minValue < 0 || maxValue < 0) {
-    alert('Значение меньше ноля');
+    alert('Значение меньше ноля'); // eslint-disable-line
     return;
   } else if (minValue > maxValue) {
-    alert('Поменяйте числа местами');
+    alert('Поменяйте числа местами'); // eslint-disable-line
   } else if (minValue === maxValue) {
-    alert('Введенные числа равны');
+    alert('Введенные числа равны'); // eslint-disable-line
   }
   return Math.floor(Math.random() * (maxValue - minValue + 1) + minValue);
 };
