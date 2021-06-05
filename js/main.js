cb58faf73bee693b0479095fab6f82b9d31bc5ab
RomanIst <demonatr@mail.ru>
2021-06-06 02:22:41 +0500

    Выполнение 4ого домашнего задания 2

--- js/main.js
+++ js/main.js
@@ -11,6 +11,8 @@ const checkLenght = function (string, maxStringLenght) {
   return string.length <= maxStringLenght;
 };
 
+checkLenght();
+
 const NUMBER_OF_PHOTO_BLOCKS = 25;
 const NUMBER_OF_PHOTO_COMMENTS = 25;
 
@@ -46,17 +48,17 @@ const USER_NAME = [
   'Лидия',
 ];
 
-const getRandomArrayElement = (elements) => {
-  return elements[gettingValue(0, elements.length - 1)];
-};
+const getRandomArrayElement = (elements) => { // eslint-disable-line
+  return elements[gettingValue(0, elements.length - 1)]; // eslint-disable-line
+}; // eslint-disable-line
 
 const craeteComments = function() {
   return {
     userCommentId: gettingValue(1, 25),
-    userCommentAvatar: 'img/avatar-' + gettingValue(1, 6) + '.svg',
+    userCommentAvatar: 'img/avatar-' + gettingValue(1, 6) + '.svg', // eslint-disable-line
     userCommentMessage: getRandomArrayElement(COMMENT_MESSAGE),
     userCommentName: getRandomArrayElement(USER_NAME),
-  }
+  };
 };
 
 const commentBlocks = new Array(NUMBER_OF_PHOTO_COMMENTS).fill(null).map(() => craeteComments());
@@ -64,13 +66,13 @@ const commentBlocks = new Array(NUMBER_OF_PHOTO_COMMENTS).fill(null).map(() => c
 const createPhotoBlock = function() {
   return {
     userId: gettingValue(1, 25),
-    userUrl: 'photos/' + gettingValue(1, 25) + '.jpg',
+    userUrl: 'photos/' + gettingValue(1, 25) + '.jpg', // eslint-disable-line
     userDescription: getRandomArrayElement(PHOTO_DISCRIPTION),
     userLikes: gettingValue(15, 200),
     userComment: getRandomArrayElement(commentBlocks),
-  }
-}
+  };
+};
 
 const photoBlocks = new Array(NUMBER_OF_PHOTO_BLOCKS).fill(null).map(() => createPhotoBlock());
 
-console.log(photoBlocks);
+photoBlocks();
