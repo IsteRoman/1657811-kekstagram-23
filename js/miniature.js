4ac6b36badb607696382fa438c032f4642ef5c7d
RomanIst <demonatr@mail.ru>
2021-06-19 01:16:14 +0500

    Дз

--- js/miniature.js
+++ js/miniature.js
@@ -1,17 +1,17 @@
-import {photoBlocks} from './data.js';
+import {blockPhoto} from './data.js';
+
 
 const showMiniature = function() {
   const photoList = document.querySelector('.pictures');
   const pictureTemplate = document.querySelector('#picture').content;
-  const miniatureData = photoBlocks();
 
   const photoListElement = document.createDocumentFragment();
 
-  miniatureData.forEach(({userUrl, userLikes}) => {
+  blockPhoto.forEach(({userUrl, userLikes, userComment}) => {
     const miniPhoto = pictureTemplate.cloneNode(true);
     miniPhoto.querySelector('.picture__img').setAttribute('src', userUrl);
     miniPhoto.querySelector('.picture__likes').textContent = userLikes;
-    miniPhoto.querySelector('.picture__comments').textContent = `${document.querySelector('.social__comments').children.length}`;
+    miniPhoto.querySelector('.picture__comments').textContent = `${userComment.length}`;
     photoListElement.appendChild(miniPhoto);
   });
 
--- js/watch.js
+++ js/watch.js
@@ -1,15 +1,15 @@
-import {commentArray} from './data.js';
 import {showMiniature} from './miniature.js';
+import {blockPhoto} from './data.js';
 
 const body = document.querySelector('body');
 const bigPicture = document.querySelector('.big-picture');
 
-const getComment = function() {
+const getComment = function(index) {
   const commentBlock = document.querySelector('.social__comments');
-  const commentTemplate = document.querySelector('.social__comment');
-  const commentData = commentArray();
-
+  const commentTemplate = document.querySelector('#comments').content;
+  const commentData = blockPhoto[index].userComment;
   const commentBlockElement = document.createDocumentFragment();
+  commentBlock.innerHTML = '';
 
   commentData.forEach(({userCommentAvatar, userCommentName, userCommentMessage}) => {
     const comment = commentTemplate.cloneNode(true);
@@ -21,7 +21,6 @@ const getComment = function() {
   });
   commentBlock.appendChild(commentBlockElement);
 };
-
 const closeByEsc = function() {
   window.addEventListener('keydown', function(evt) {  // eslint-disable-line
     if (evt.keyCode === 27) {  // eslint-disable-line
@@ -40,25 +39,28 @@ const closeByButton = function() {
 };
 
 const browse = function() {
+
   showMiniature();
-  getComment();
   const picture = document.querySelectorAll('.picture');
   const commentsCount = document.querySelector('.social__comment-count');
   const commentsLoader = document.querySelector('.comments-loader');
-  picture.forEach(function(showBigPictuer) {   // eslint-disable-line
-    showBigPictuer.addEventListener('click', openBigPicture);  // eslint-disable-line
+  picture.forEach(function(showBigPictuer, index) {   // eslint-disable-line
+    showBigPictuer.addEventListener('click', openBigPicture.bind(showBigPictuer, index));  // eslint-disable-line
   });  // eslint-disable-line
-  function openBigPicture(event) {
+  function openBigPicture(index, event) {
     event.preventDefault();
+    getComment(index);
 
     const likes = document.querySelector('.likes-count');
     const pictureUrl = document.querySelector('.big-picture__img').
       querySelector('img');
-    const commentsNumber = document.querySelector('.comments-count');  // eslint-disable-line
-    const discription = document.querySelector('.social__caption'); // eslint-disable-line
+    const discription = document.querySelector('.social__caption');
+    const sd = blockPhoto[index].userDescription;
+    discription.textContent = sd;
 
     body.classList.add('modal-open');
     bigPicture.classList.remove('hidden');
+
     const allLikes = this.querySelector('.picture__likes');
     const allUrls = this.querySelector('.picture__img');
     const allUrl = allUrls.getAttribute('src');
